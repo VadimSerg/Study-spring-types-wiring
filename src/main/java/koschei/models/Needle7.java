f604c5cfca98403017b0e0a5d99b645986985dc9
VaDIM <33179761+VadimSerg@users.noreply.github.com>
2020-12-30 07:44:29 +1000

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,16 @@
+package koschei.models;
+
+import org.springframework.stereotype.Component;
+
+
+public class Needle7 {
+
+
+    public Needle7(Deth8 deth) {
+    }
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + "";
+    }
+}
